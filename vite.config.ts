2c5599eeb1eded9b44ee7904ed6871429971b521
narip <narip@github.com>
2024-04-25 22:42:23 +0900

    feat: vitest 설정, 스크립트 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+  },
+  server: {
+    port: 3000,
+    /** 포트가 이미 사용 중일 때 다른 포트를 사용하게끔 설정 */
+    strictPort: false
+  }
 })
