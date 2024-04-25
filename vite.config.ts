f55b4e0a9980f784f13f89c5a7411032e1a38999
narip <narip@github.com>
2024-04-25 23:22:22 +0900

    refactor: eslint, prettier 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +11,6 @@ export default defineConfig({
   server: {
     port: 3000,
     /** 포트가 이미 사용 중일 때 다른 포트를 사용하게끔 설정 */
-    strictPort: false
-  }
-})
+    strictPort: false,
+  },
+});
